53151e29093218ba778923522559067c118f7984
Julio Mendes <juliomendes160@hotmail.com>
2026-02-25 16:08:00 -0300

    Final commit

---
+++ .vscode/.bash
@@ -0,0 +1,7 @@
+BASH_HOME="C:\Program Files\Git\bin"
+CMD_HOME="C:\WINDOWS\system32"
+POWERSHELL_HOME="C:\WINDOWS\System32\WindowsPowerShell\v1.0"
+CODE_HOME="C:\Program Files\Microsoft VS Code\bin"
+GIT_HOME="C:\Program Files\Git\cmd"
+NODE_HOME="F:\dev\node\node-v22.18.0-win-x64"
+PATH=$(/bin/cygpath -p "$BASH_HOME;$CMD_HOME;$POWERSHELL_HOME;$CODE_HOME;$GIT_HOME;$NODE_HOME")
